8b9a1cf0ab9016f696ad00f12ab0065bb82a8e3d
Austin Willoughby <arwigm@rit.edu>
2025-10-15 13:49:49 -0400

    package updates

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
 
 const router = require('./router.js');
 
@@ -14,8 +13,8 @@ app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 
 app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 router(app);
 
